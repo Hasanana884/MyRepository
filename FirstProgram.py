237a0c43d66d558de0ad6db6b40ac73aed073248
Hasanana884 <106733534+Hasanana884@users.noreply.github.com>
2022-06-03 11:03:05 +0530

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -3,7 +3,7 @@ print("When you do some modification in file please mention your name as company
 lst=[1,2,3,4,5,6,7,8,9,10]
 for i in lst:
 	print(i)
-
+        print(i)
 print("I have done some modification")
 print("sum ",sum(lst))
 print("Length ",len(lst))
